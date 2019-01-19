3ecebaa5d03336505f6efeecba222b43b408ee4c
Joshua Heller <joshua@torfeld6.com>
2019-01-19 14:08:53 +0100

    add setup.py for first pypi package tests

---
+++ setup.py
@@ -0,0 +1,36 @@
+# This is purely the result of trial and error.
+
+from setuptools import setup, find_packages
+
+import kudu
+
+install_requires = [
+    'requests>=2.21.0',
+    'GitPython>=2.1.11',
+    'paramiko>=2.4.2'
+]
+
+# bdist_wheel
+extras_require = {
+    # http://wheel.readthedocs.io/en/latest/#defining-conditional-dependencies
+    'python_version == "3.0" or python_version == "3.1"': ['argparse>=1.2.1'],
+}
+
+
+def long_description():
+    return ''
+
+
+setup(
+    name='kudu',
+    version=kudu.__version__,
+    author=kudu.__author__,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'kudu = kudu.__main__:main',
+        ],
+    },
+    extras_require=extras_require,
+    install_requires=install_requires
+)
