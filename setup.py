c3d44f2c53f32172879beb65bbd6671d978dabff
Joshua Heller <joshua@torfeld6.com>
2019-02-20 12:13:14 +0100

    fixes entry_points

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from kudu import __author__
 from kudu import __email__
@@ -6,11 +6,11 @@ from kudu import __version__
 
 setup(
     name='kudu',
+    author=__author__,
+    author_email=__email__,
     version=__version__,
     description='A deployment command line program in Python.',
     url='https://github.com/torfeld6/kudu-cli',
-    author=__author__,
-    author_email=__email__,
     license='BSD',
     classifiers=[
         'Intended Audience :: Developers',
@@ -27,10 +27,11 @@ setup(
         'Programming Language :: Python :: 3.4',
     ],
     keywords='cli',
-    install_requires=['requests', 'PyYAML', 'GitPython', 'watchdog', 'click', 'anyconfig'],
+    packages=find_packages(),
+    install_requires=['requests', 'PyYAML', 'GitPython', 'watchdog', 'click'],
     entry_points={
         'console_scripts': [
-            'kudu = kudu.__main__:main',
+            'kudu = kudu.__main__:cli',
         ],
     },
 )
