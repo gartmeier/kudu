a803c58ee50255309e7265fb1be28752fd58567c
Joshua Heller <joshua@torfeld6.com>
2019-10-28 16:01:08 +0100

    implements push path option #3

--- kudu/commands/push.py
+++ kudu/commands/push.py
@@ -11,25 +11,54 @@ from kudu.config import ConfigOption
 from kudu.types import PitcherFileType
 
 
+def make_archive(base_name, root_dir):
+    save_cwd = os.getcwd()
+    os.chdir(root_dir)
+
+    th, tp = tempfile.mkstemp('.zip')
+    zf = zipfile.ZipFile(tp, 'w', zipfile.ZIP_DEFLATED)
+
+    for root, dirs, files in os.walk(os.getcwd()):
+        arcroot = os.path.relpath(root)
+        for name in files:
+            arcname = name if not (arcroot == os.curdir and name == 'thumbnail.png') else base_name + '.png'
+            zf.write(os.path.join(root, name), os.path.join(base_name, arcroot, arcname))
+
+    zf.close()
+    os.chdir(save_cwd)
+
+    return th, tp
+
+
 @click.command()
-@click.option('--file', '-f', 'pfile',
-              cls=ConfigOption, config_name='file_id',
-              prompt=True, type=PitcherFileType(category=('zip', 'presentation')))
+@click.option(
+    '--file', '-f', 'pf',
+    cls=ConfigOption,
+    config_name='file_id',
+    prompt=True,
+    type=PitcherFileType(
+        category=('zip', 'presentation', 'json')
+    )
+)
+@click.option(
+    '--path', '-p',
+    default=lambda: os.getcwd()
+)
 @click.pass_context
-def push(ctx, pfile):
-    chroot, ext = os.path.splitext(pfile['filename'])
+def push(ctx, pf, path):
+    filename, ext = os.path.splitext(pf['filename'])
+    upload_url = api_request('get', '/files/%d/upload-url/' % pf['id'], token=ctx.obj['token']).json()
 
-    with tempfile.NamedTemporaryFile() as tf:
-        zf = zipfile.ZipFile(tf.name, 'w', zipfile.ZIP_DEFLATED)
-        for root, dirs, files in os.walk(os.getcwd()):
-            arcroot = os.path.relpath(root)
-            for name in files:
-                arcname = name if not (arcroot == os.curdir and name == 'thumbnail.png') else chroot + '.png'
-                zf.write(os.path.join(root, name), os.path.join(chroot, arcroot, arcname))
-        zf.close()
+    if os.path.isdir(path):
+        th, tp = make_archive(filename, path)
+        requests.put(upload_url, data=os.fdopen(th, 'r+b'))
+        os.remove(tp)
+    else:
+        requests.put(upload_url, data=open(path, 'r+b'))
 
-        upload_url = api_request('get', '/files/%d/upload-url/' % pfile['id'], token=ctx.obj['token']).json()
-        requests.put(upload_url, data=tf)
-
-    api_request('patch', '/files/%d/' % pfile['id'], json={'creationTime': datetime.utcnow().isoformat()},
-                token=ctx.obj['token'])
+    api_request(
+        'patch',
+        '/files/%d/' % pf['id'],
+        json={'creationTime': datetime.utcnow().isoformat()},
+        token=ctx.obj['token']
+    )
